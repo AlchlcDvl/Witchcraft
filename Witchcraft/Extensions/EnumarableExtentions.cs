cfbfa5f90b9960237828f05be4364a023715f514
AD <56699741+AlchlcDvl@users.noreply.github.com>
2023-09-07 11:26:40 -0400

    some more functions

--- Witchcraft/Extensions/EnumarableExtentions.cs
+++ Witchcraft/Extensions/EnumarableExtentions.cs
@@ -19,7 +19,7 @@ public static class EnumerableExtentions
 
         for (var i = list.Count - 1; i > 0; --i)
         {
-            var r = URandom.Range(0, i + 1);
+            var r = URandom.RandomRangeInt(0, i + 1);
             (list[r], list[i]) = (list[i], list[r]);
         }
     }
@@ -38,28 +38,55 @@ public static class EnumerableExtentions
         }
     }
 
-    public static void RemoveRange<T>(this List<T> list, IEnumerable<T> list2)
+    public static int RemoveRange<T>(this List<T> list, IEnumerable<T> list2)
     {
+        var result = 0;
+
         foreach (var item in list2)
         {
             if (list.Contains(item))
-                list.Remove(item);
+                result += list.RemoveAll(x => Equals(x, item));
         }
+
+        return result;
     }
 
     public static void AddRanges<T>(this List<T> main, params IEnumerable<T>[] items) => items.ForEach(main.AddRange);
 
-    public static void RemoveRanges<T>(this List<T> main, params IEnumerable<T>[] items) => items.ForEach(main.RemoveRange);
+    public static int RemoveRanges<T>(this List<T> main, params IEnumerable<T>[] items)
+    {
+        var result = 0;
+        items.ForEach(x => result += main.RemoveRange(x));
+        return result;
+    }
 
-    public static bool Replace<T>(this List<T> list, T item1, T item2)
+    public static bool Replace<T>(this List<T> list, T item1, T item2, bool all)
     {
         var contains = list.Contains(item1);
 
         if (contains)
         {
-            var index = list.IndexOf(item1);
-            list.Remove(item1);
-            list.Insert(index, item2);
+            if (all)
+            {
+                var pos = 0;
+                var clone = list;
+
+                foreach (var item in clone)
+                {
+                    if (Equals(item, item1))
+                    {
+                        pos = clone.IndexOf(item);
+                        list.Remove(item);
+                        list.Insert(pos, item2);
+                    }
+                }
+            }
+            else
+            {
+                var index = list.IndexOf(item1);
+                list.Remove(item1);
+                list.Insert(index, item2);
+            }
         }
 
         return contains;
@@ -73,5 +100,11 @@ public static class EnumerableExtentions
             return list[0];
         else
             return list[URandom.RandomRangeInt(0, list.Count)];
+    }
+
+    public static void ForEach<TKey, TValue>(this IDictionary<TKey, TValue> dict, Action<TKey, TValue> action)
+    {
+        foreach (var pair in dict)
+            action(pair.Key, pair.Value);
     }
 }
