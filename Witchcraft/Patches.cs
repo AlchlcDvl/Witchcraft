6668ee4882a1cc0c58be574b3f413981919cb9b2
AD <56699741+AlchlcDvl@users.noreply.github.com>
2024-01-28 18:12:58 -0500

    forgot about my copy paste

--- Witchcraft/Patches.cs
+++ Witchcraft/Patches.cs
@@ -30,18 +30,17 @@ public static class Patches
             __instance.roleInfoLookup[Role.MARSHAL].sprite = Witchcraft.Marshal;
             __instance.roleInfoLookup[Role.SOCIALITE].sprite = Witchcraft.Socialite;
             __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
-            __instance.roleInfoLookup[Role.GHOST_TOWN] = CreateNewInfo(Witchcraft.GhostTown!, Role.GHOST_TOWN, "GT");
+            __instance.roleInfoLookup[Role.VIP] = CreateNewInfo(Witchcraft.VIP!, Role.VIP, "VIP");
+            __instance.roleInfoLookup[Role.TOWN_TRAITOR] = CreateNewInfo(Witchcraft.TownTraitor!, Role.TOWN_TRAITOR, "TT");
+            __instance.roleInfoLookup[Role.NO_TOWN_HANGED] = CreateNewInfo(Witchcraft.PerfectTown!, Role.NO_TOWN_HANGED, "PT");
+            __instance.roleInfoLookup[Role.SLOW_MODE] = CreateNewInfo(Witchcraft.SlowMode!, Role.SLOW_MODE, "SM");
+            __instance.roleInfoLookup[Role.FAST_MODE] = CreateNewInfo(Witchcraft.FastMode!, Role.FAST_MODE, "FM");
+            __instance.roleInfoLookup[Role.ANONYMOUS_VOTES] = CreateNewInfo(Witchcraft.AnonVotes!, Role.ANONYMOUS_VOTES, "AV");
+            __instance.roleInfoLookup[Role.KILLER_ROLES_HIDDEN] = CreateNewInfo(Witchcraft.SecretKillers!, Role.KILLER_ROLES_HIDDEN, "SeK");
+            __instance.roleInfoLookup[Role.ROLES_ON_DEATH_HIDDEN] = CreateNewInfo(Witchcraft.HiddenRoles!, Role.ROLES_ON_DEATH_HIDDEN, "HR");
+            __instance.roleInfoLookup[Role.ONE_TRIAL_PER_DAY] = CreateNewInfo(Witchcraft.OneTrial!, Role.ONE_TRIAL_PER_DAY, "OT");
+            __instance.roleInfoLookup[Role.HIDDEN] = CreateNewInfo(Witchcraft.Hidden!, Role.HIDDEN, "Hidden");
+            __instance.roleInfoLookup[Role.STONED] = CreateNewInfo(Witchcraft.Stoned!, Role.STONED, "Stoned");
         }
 
         private static RoleInfo CreateNewInfo(Sprite sprite, Role role, string shortRoleName) => new()
