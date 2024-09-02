289f7c20b53b0dfba211d7ef204dd4aa00721c40
AlchlcDvl <56699741+AlchlcDvl@users.noreply.github.com>
2024-09-02 18:16:42 -0400

    forgot to commit this

--- Witchcraft/Utils/UnityUtils.cs
+++ Witchcraft/Utils/UnityUtils.cs
@@ -1,6 +1,6 @@
 namespace Witchcraft.Utils;
 
-public static class UnityObjectUtils
+public static class UnityUtils
 {
     public static T DontDestroy<T>(this T obj) where T : UObject
     {
@@ -23,4 +23,12 @@ public static class UnityObjectUtils
     public static void Destroy(this UObject obj) => UObject.Destroy(obj);
 
     public static void DestroyImmediate(this UObject obj) => UObject.DestroyImmediate(obj);
+
+    public static T AddComponent<T>(this Component component) where T : Component => component?.gameObject?.AddComponent<T>()!;
+
+    public static T GetComponent<T>(this Component component) where T : Component => component?.gameObject?.GetComponent<T>()!;
+
+    public static T EnsureComponent<T>(this Component component) where T : Component => component?.gameObject?.EnsureComponent<T>()!;
+
+    public static T EnsureComponent<T>(this GameObject gameObject) where T : Component => gameObject?.GetComponent<T>()! ?? gameObject?.AddComponent<T>()!;
 }
