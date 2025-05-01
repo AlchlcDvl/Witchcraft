317782a5eb298b4364dc6de80d503c0a4e2565d5
AlchlcDvl <56699741+AlchlcDvl@users.noreply.github.com>
2025-05-01 19:00:09 -0400

    Slight fix

--- Witchcraft/Witchcraft.cs
+++ Witchcraft/Witchcraft.cs
@@ -5,11 +5,12 @@ namespace Witchcraft;
 public class Witchcraft : BaseMod<Witchcraft>
 {
     public override string Name => "Witchcraft";
+    public override string HarmonyId => "alchlcsystm.witchcraft";
     public override bool HasFolder => true;
 
     public override void Start() => Message("Magic is brewing!", true);
 
-    public override void UponAssetsLoaded() => MenuButton.LogsButton.Icon = Assets.GetSprite("Thumbnail");
+    public override void UponAssetsHandled() => MenuButton.LogsButton.Icon = Assets.GetSprite("Thumbnail");
 }
 
 [SalemMenuItem]
